314ff6b33eea740700bfa7f5b6576f79e3f5fd8e
Louis-Philippe Bosse <louisphilippe.bosse@bell.ca>
2024-08-22 18:46:57 -0400

    Will need to test with all these variables

--- reference_implementations/gcp/architectures/online/main.tf
+++ reference_implementations/gcp/architectures/online/main.tf
@@ -39,6 +39,11 @@ variable "schemas_folder" {
   type = string
 }
 
+variable "team_members" {
+  description = "List of team member identifiers"
+  type        = list(string)
+}
+
 locals {
   # cleaning up project name to make it friendly to some IDs
   project_prefix = replace(var.project, "-", "_")
@@ -233,4 +238,100 @@ resource "google_vertex_ai_featurestore_entitytype_feature" "data_feature" {
   name       = "data_feature"
   entitytype = google_vertex_ai_featurestore_entitytype.data_entity.id
   value_type = "STRING"
+}
+
+resource "google_compute_network" "my_network" {
+  name                    = "${var.short_project_prefix}-network"
+  auto_create_subnetworks = false
+  project                 = var.project
+}
+
+resource "google_compute_subnetwork" "my_subnetwork" {
+  name          = "${var.short_project_prefix}-subnetwork"
+  network       = google_compute_network.my_network.id
+  region        = var.region
+  ip_cidr_range = "10.0.1.0/24"
+  project       = var.project
+}
+
+resource "google_compute_address" "static" {
+  name    = "${var.short_project_prefix}-static-ip"
+  region  = var.region
+  project = var.project
+}
+
+resource "google_service_account_iam_binding" "act_as_permission" {
+  service_account_id = "projects/${var.project}/serviceAccounts/${var.user}"
+  role               = "roles/iam.serviceAccountUser"
+  members            = [for member in var.team_members : "user:${member}"]
+}
+
+resource "google_workbench_instance" "instance" {
+  for_each = toset(var.team_members)
+
+  name     = "${var.short_project_prefix}-workbench-${each.key}"
+  location = "${var.region}-a"
+  project  = var.project
+
+  gce_setup {
+    machine_type = "n1-standard-4"
+    accelerator_configs {
+      type       = "NVIDIA_TESLA_T4"
+      core_count = 1
+    }
+
+    shielded_instance_config {
+      enable_secure_boot         = true
+      enable_vtpm                = true
+      enable_integrity_monitoring = true
+    }
+
+    disable_public_ip = false
+
+    service_accounts {
+      email = "${each.value}@service-account.com" # Example of dynamic email construction
+    }
+
+    boot_disk {
+      disk_size_gb    = 310
+      disk_type       = "PD_SSD"
+      disk_encryption = "CMEK"
+      kms_key         = "my-crypto-key"
+    }
+
+    data_disks {
+      disk_size_gb    = 330
+      disk_type       = "PD_SSD"
+      disk_encryption = "CMEK"
+      kms_key         = "my-crypto-key"
+    }
+
+    network_interfaces {
+      network      = google_compute_network.my_network.id
+      subnet       = google_compute_subnetwork.my_subnetwork.id
+      nic_type     = "GVNIC"
+      access_configs {
+        external_ip = google_compute_address.static.address
+      }
+    }
+
+    metadata = {
+      terraform = "true"
+    }
+
+    enable_ip_forwarding = true
+
+    tags = ["${var.short_project_prefix}-${each.key}"]
+  }
+
+  disable_proxy_access = true
+
+  instance_owners = [each.value]
+
+  labels = {
+    project = var.project
+    env     = var.env
+  }
+
+  desired_state = "ACTIVE"
 }
