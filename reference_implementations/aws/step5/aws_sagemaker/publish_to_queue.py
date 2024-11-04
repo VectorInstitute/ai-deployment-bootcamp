d16c24f580b567d558133a1359c1d0d5448a8c71
Roya Javadi <roya.javadi@vectorinstitute.ai>
2024-11-04 15:33:42 -0800

    Get id from args

--- reference_implementations/aws/step5/aws_sagemaker/publish_to_queue.py
+++ reference_implementations/aws/step5/aws_sagemaker/publish_to_queue.py
@@ -1,7 +1,9 @@
 import boto3
-import logging
 import json
+import logging
+import argparse
 
+# Function to publish message to SQS
 def publish_message_to_sqs(queue_url, message_body):
     sqs_client = boto3.client('sqs')
     response = sqs_client.send_message(
@@ -10,18 +12,22 @@ def publish_message_to_sqs(queue_url, message_body):
     )
     return response['MessageId']
 
-
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-queue_url = 'https://sqs.us-east-1.amazonaws.com/025066243062/my_queue'
-# input_data = {
-#         "seq_0": "The cat chased the mouse.",
-#         "seq_1": "The mouse was chased by the cat."
-#     }
+# Parse the ID from command line arguments
+parser = argparse.ArgumentParser(description="Publish message to SQS with a given ID.")
+parser.add_argument("id", type=str, help="The ID to include in the SQS message.")
+args = parser.parse_args()
 
+# SQS queue URL
+queue_url = 'https://sqs.us-east-1.amazonaws.com/025066243062/my_queue'
+
+# Prepare input data
 input_data = {
-        "id": "2"
-    }
+    "id": args.id  # Get the ID from command-line argument
+}
 
 # Convert input data to JSON string
 payload = json.dumps(input_data)
