d9948b3c3f819a344dce12d1c6601694f2f4d0b4
Roya Javadi <roya.javadi@vectorinstitute.ai>
2024-11-08 08:03:09 -0800

    clean up

--- reference_implementations/aws/step1/ml-api/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.get("/predict")
-async def predict():
-    return {"message": "mock prediction"}
--- reference_implementations/gcp/step3/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-project = "ai-deployment-bootcamp"
-region = "us-west2"
-user = "marcelolotif"
-scriptpath = "./ml-api/startup.sh"
